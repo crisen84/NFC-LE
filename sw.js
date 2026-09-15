9124588f3ec50a43fb627a2532c702bcbc2b8f36
crisen84 <crisen84@msn.com>
2026-09-15 00:51:51 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'nfc-v3';
+const ARCHIVOS = ['./', './manifest.json', './icono-192.png', './icono-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS).catch(() => {})));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )).then(() => self.clients.claim()));
+});
+
+// La pagina siempre se busca primero en la red, para que una version nueva
+// se vea en cuanto se sube. Si no hay senal, se usa la copia guardada.
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copia = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copia)).catch(() => {});
+        return r;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./')))
+  );
+});
